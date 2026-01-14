7c4e2229b4139a78d6d2d2bb83335c2c94937c3d
Josiah Welch <jwelch11@huskers.unl.edu>
2026-01-14 14:49:39 -0600

    created TokenType and TokenCategory enumerated types

--- structs.h
+++ structs.h
@@ -1,2 +1,23 @@
-struct Token {
+enum TokenCat {
+	KEYWORD,
+	IDENTIFIER,
+	CONSTANT,
+	OPERATOR,
+	BRACKET,
+	SEPARATOR
+};
 
+enum TokenType {
+	STR,
+	INT,
+	FLT,
+	CHR,
+	LOG,
+	CMP,
+	MAT
+};
+
+struct Token {
+	enum TokenCat category;
+	enum TokenType type;
+	char 
